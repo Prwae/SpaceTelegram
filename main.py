4c731eaf82a3fa35c05f229e4fc05b25bdaf4eb9
Prytkovkach <odprytkov1@gmail.com>
2021-11-15 18:34:09 +0300

    Удалил отладочный принт

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_apod(count, apikey, directory):
         "apikey": apikey,
         "count": count
     }
-    print(f"https://api.nasa.gov/planetary/apod?api_key={apikey}&count={count}")
+    
     response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={apikey}&count={count}")
     response.raise_for_status()
 
