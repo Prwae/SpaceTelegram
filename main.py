58938667bc6777a0110c0d62e5109551b1c9250e
Prytkovkach <odprytkov1@gmail.com>
2022-08-04 18:58:00 +0300

    Крупные правки
    -

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlsplit, unquote
+from urllib.parse import urlparse
 import os
 import time
 import random
@@ -6,24 +6,26 @@ import requests
 import telegram
 from dotenv import load_dotenv
 import argparse
+from pprint import pprint
+import datetime
 
 
-def download_image(url, way, params=None):
+def download_image(url, path, params=None):
     response = requests.get(url, params=params)
     response.raise_for_status()
 
-    with open(way, "wb") as file:
+    with open(path, "wb") as file:
         file.write(response.content)
 
 
 def get_file_extension(link):
-    return os.path.splitext(urlsplit(unquote(link))[2])[1]
+    return os.path.splitext(urlparse(link).path)[1]
 
 
 def fetch_spacex_last_launch(directory):
-    response = requests.get("https://api.spacexdata.com/v4/launches/latest")
+    response = requests.get("https://api.spacexdata.com/v3/launches/64")
     response.raise_for_status()
-
+    pprint(response.json())
     for img_number, image in enumerate(response.json()["links"]["flickr_images"]):
         download_image(image, f"{directory}spacex{img_number}.jpeg")
 
@@ -50,7 +52,9 @@ def get_epic(apikey, directory):
     response.raise_for_status()
 
     for img_id, epic in enumerate(response.json()):
-        url = (f"https://api.nasa.gov/EPIC/archive/natural/{epic['date'][0:10].replace('-', '/')}/png/{epic['image']}.png")
+        datetime_date = datetime.date.fromisoformat(epic["date"])
+        formatted_date = datetime_date.strftime("%Y/%m/%d")
+        url = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{epic['image']}.png"
         download_image(url, f"{directory}epic{img_id}.jpeg", payload)
 
 
@@ -64,8 +68,8 @@ def post_images_to_telegram(directory, chat_id, image_send_peridiocity):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('image_send_periodicity', help='Периодичность отправки фото')
-    image_send_periodicity = parser.parse_args()
+    parser.add_argument('image_send_periodicity', help='Периодичность отправки фото', type=int)
+    image_send_periodicity = parser.parse_args().image_send_periodicity
 
     os.makedirs("images", exist_ok=True)
     load_dotenv()
@@ -74,7 +78,4 @@ if __name__ == "__main__":
     chat_id = os.environ["TELEGRAM_CHANNEL"]
     bot = telegram.Bot(token=telegram_token)
 
-    fetch_spacex_last_launch("images/")
-    get_apod(30, nasa_token, "images/")
-    get_epic(nasa_token, "images/")
-    post_images_to_telegram("images/", chat_id, (image_send_periodicity.image_send_periodicity))
+    post_images_to_telegram("images/", chat_id, image_send_periodicity)
