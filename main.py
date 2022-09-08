0cd56c0d445c5f6344007137addf691eb10bf21c
Prytkovkach <odprytkov1@gmail.com>
2022-09-08 18:48:35 +0300

    Уточнил название переменной
    Теперь более понятно, что в ней хранится

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import argparse
 
 def post_images_to_telegram(directory, chat_id, sec_delay):
     while True:
-        image_choice = random.choice(os.listdir("images"))
+        image_path = random.choice(os.listdir("images"))
         try:
-            with open(os.path.join(directory, image_choice), "rb") as file:
+            with open(os.path.join(directory, image_path), "rb") as file:
                 bot.send_photo(chat_id=chat_id, photo=file)
             time.sleep(sec_delay)
         except telegram.error.NetworkError:
